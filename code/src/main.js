5af18d5d40fdeb46132bf7e9e564200cb0727016
Gavin Wen <349088304@qq.com>
2019-02-21 16:40:47 +0800

    add cube-ui

--- code/src/main.js
+++ code/src/main.js
@@ -1,7 +1,10 @@
 import Vue from 'vue'
 import App from './App.vue'
+import Cube from 'cube-ui'
 
 Vue.config.productionTip = false
+
+Vue.use(Cube)
 
 new Vue({
   render: h => h(App)
---
+++ code/vue.config.js
@@ -0,0 +1,19 @@
+const PostCompilePlugin = require('webpack-post-compile-plugin')
+
+module.exports = {
+  chainWebpack: config => {
+    const conf = config.toConfig()
+    config
+      .plugin('post-compile')
+      .use(PostCompilePlugin)
+  },
+  css: {
+    loaderOptions: {
+      stylus: {
+        'resolve url': true
+        // 自定义主题场景
+        // import: [path.resolve(__dirname, './src/theme')]
+      }
+    }
+  }
+}
